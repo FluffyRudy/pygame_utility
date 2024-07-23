039782879af43a5ce505a542d9e2c5017b3d8dfe
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-07-23 18:26:13 +0545

    update theme

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -22,7 +22,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
     "sphinx.ext.todo",
-    "sphinx_rtd_theme",
+    "sphinx_rtd_dark_mode",
 ]
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
@@ -31,5 +31,5 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_rtd_dark_mode"
 html_static_path = ["_static"]
