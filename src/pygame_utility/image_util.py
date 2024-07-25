f2a5aa1d0c87228aec8b77aab66d15774f1f6c41
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-07-25 20:14:57 +0545

    Correct precedence of `scale_size` over `scale_ratio'

--- src/pygame_utility/image_util.py
+++ src/pygame_utility/image_util.py
@@ -6,26 +6,27 @@ from typing import Optional, Union, Tuple, List
 def load_image(
     path: Union[str, Path],
     scale_ratio: Tuple[float, float] = (1.0, 1.0),
-    scale_size: Optional[Tuple[float, float]] = None,
+    scale_size: Optional[Tuple[int, int]] = None,
 ) -> Optional[pygame.Surface]:
     """
     Load an image from the given path and return it as a pygame Surface, scaled by the given ratio or size.
 
     Args:
-        path (str): Path of the image file.
-        scale_ratio (Optional[Tuple[float, float]]): Tuple representing the scaling ratio for width and height. Takes priority over scale_size if provided.
-        scale_size (Optional[Tuple[float, float]]): Tuple representing the target width and height in pixels. Used if scale_ratio is None.
+        path (Union[str, Path]): Path of the image file.
+        scale_ratio (Tuple[float, float]): Tuple representing the scaling ratio for width and height. Defaults to (1.0, 1.0).
+        scale_size (Optional[Tuple[int, int]]): Tuple representing the target width and height in pixels.
+            If provided, `scale_size` takes priority over `scale_ratio`.
 
     Returns:
-        pygame.Surface: The loaded and scaled image as a pygame Surface object.
+        Optional[pygame.Surface]: The loaded and scaled image as a pygame Surface object, or None if loading fails.
     """
     image = None
     try:
         image = pygame.image.load(str(path)).convert_alpha()
-        if scale_ratio:
-            image = pygame.transform.scale_by(image, scale_ratio)
-        elif scale_size:
+        if scale_size:
             image = pygame.transform.scale(image, scale_size)
+        elif scale_ratio != (1.0, 1.0):
+            image = pygame.transform.scale_by(image, scale_ratio)
     except Exception as error:
         print(error)
     finally:
@@ -35,19 +36,20 @@ def load_image(
 def load_state_frames(
     path: Union[str, Path],
     scale_ratio: Tuple[float, float] = (1.0, 1.0),
-    scale_size: Optional[Tuple[float, float]] = None,
-) -> dict[str, List[pygame.Surface]]:
+    scale_size: Optional[Tuple[int, int]] = None,
+) -> dict[str, List[Optional[pygame.Surface]]]:
     """
     Load multiple frames (images) from subdirectories of the given path and return them as a dictionary of Lists of pygame Surfaces.
     The images within each subdirectory are loaded in alphabetical order.
 
     Args:
-        path (str): Path of the directory containing subdirectories of image files.
-        scale_ratio (Optional[Tuple[float, float]]): Tuple representing the scaling ratio for width and height. Takes priority over scale_size if provided.
-        scale_size (Optional[Tuple[float, float]]): Tuple representing the target width and height in pixels. Used if scale_ratio is None.
+        path (Union[str, Path]): Path of the directory containing subdirectories of image files.
+        scale_ratio (Tuple[float, float]): Tuple representing the scaling ratio for width and height. Defaults to (1.0, 1.0).
+        scale_size (Optional[Tuple[int, int]]): Tuple representing the target width and height in pixels.
+            If provided, `scale_size` takes priority over `scale_ratio`.
 
     Returns:
-        dict[str, List[pygame.Surface]]: A dictionary where keys are subdirectory names and values are Lists of loaded and scaled frames as pygame Surface objects.
+        dict[str, List[Optional[pygame.Surface]]]: A dictionary where keys are subdirectory names and values are Lists of loaded and scaled frames as pygame Surface objects.
     """
     frames = {}
 
@@ -55,14 +57,15 @@ def load_state_frames(
         return frames
 
     for subdir in sorted(Path(path).iterdir()):
-        frame_name = subdir.name
-        frames[frame_name] = []
-        for file in subdir.iterdir():
-            if not file.suffix == ".png":
-                print(f"WARNING: {file.name} is not .png file")
-                continue
-            surface = load_image(file, scale_ratio, scale_size)
-            frames[frame_name].append(surface)
+        if subdir.is_dir():
+            frame_name = subdir.name
+            frames[frame_name] = []
+            for file in sorted(subdir.iterdir()):
+                if file.suffix != ".png":
+                    print(f"WARNING: {file.name} is not a .png file")
+                    continue
+                surface = load_image(file, scale_ratio, scale_size)
+                frames[frame_name].append(surface)
 
     return frames
 
@@ -70,19 +73,20 @@ def load_state_frames(
 def load_frames(
     path: Union[str, Path],
     scale_ratio: Tuple[float, float] = (1.0, 1.0),
-    scale_size: Optional[Tuple[float, float]] = None,
-) -> List[pygame.Surface]:
+    scale_size: Optional[Tuple[int, int]] = None,
+) -> List[Optional[pygame.Surface]]:
     """
     Load frames (images) from the given path and return them as a List of pygame Surfaces.
-    The images within directory are loaded in alphabetical order.
+    The images within the directory are loaded in alphabetical order.
 
     Args:
-        path (str): Path of the directory containing image files.
-        scale_ratio (Optional[Tuple[float, float]]): Tuple representing the scaling ratio for width and height. Takes priority over scale_size if provided.
-        scale_size (Optional[Tuple[float, float]]): Tuple representing the target width and height in pixels. Used if scale_ratio is None.
+        path (Union[str, Path]): Path of the directory containing image files.
+        scale_ratio (Tuple[float, float]): Tuple representing the scaling ratio for width and height. Defaults to (1.0, 1.0).
+        scale_size (Optional[Tuple[int, int]]): Tuple representing the target width and height in pixels.
+            If provided, `scale_size` takes priority over `scale_ratio`.
 
     Returns:
-        List[pygame.Surface]: A List of loaded and scaled frames as pygame Surface objects.
+        List[Optional[pygame.Surface]]: A List of loaded and scaled frames as pygame Surface objects.
     """
     frames = []
 
@@ -90,8 +94,8 @@ def load_frames(
         return frames
 
     for file in sorted(Path(path).iterdir()):
-        if not file.suffix == ".png":
-            print(f"WARNING: {file.name} is not .png file")
+        if file.suffix != ".png":
+            print(f"WARNING: {file.name} is not a .png file")
             continue
         surface = load_image(file, scale_ratio, scale_size)
         frames.append(surface)
