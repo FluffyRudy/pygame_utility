e757c044ed70671e7c60865584e75af410938cd5
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-07-25 20:27:49 +0545

    update package version

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "Pygame Utility"
 copyright = "2024, FluffyRudy"
 author = "FluffyRudy"
-release = "0.1.2"
+release = "0.1.3"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
