a40fc4bcb4c7f942e191de92cdf4d27e5c226393
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-08-18 19:26:50 +0545

    remove python2.6 from requirements

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "Pygame Utility"
 copyright = "2024, FluffyRudy"
 author = "FluffyRudy"
-release = "0.3.4"
+release = "0.3.5"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
